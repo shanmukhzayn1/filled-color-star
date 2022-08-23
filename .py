4d5e0cb8055ea1fe395a87911c91a1a06b914b64
SHANMUKHA Gopagoni <67545133+shanmukhzayn1@users.noreply.github.com>
2022-08-23 23:29:27 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,9 @@
+import turtle   #module
+wn = turtle.Screen() #Screen 
+alex  = turtle.Turtle()  # Naming the turtle 
+alex.fillcolor("dark")   #filling the color 
+alex.begin_fill()         # color filling starts 
+for _ in range(6):         #for loop
+  alex.forward(189)
+  alex.right(144)
+alex.end_fill()  
